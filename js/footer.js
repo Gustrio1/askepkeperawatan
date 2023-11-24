64bea0b0efebd25f2f8b0510f9b51e77b78d4028
Gustrio1 <riosademo@gmail.com>
2023-11-24 22:52:24 +0700

    first commit

---
+++ js/footer.js
@@ -0,0 +1,23 @@
+const footer = document.getElementById("footerEnd");
+footer.innerHTML = `<div class="footer">
+<div class="container">		
+  <div class="row text-center">						
+    <div class="col-lg-12 col-sm-12 col-xs-12">
+      <div class="footer_menu">
+      </div>						
+      <div class="footer_copyright">
+        <p>© 2023 Keperawatan Kelas E</p>
+      </div>	
+      <div class="footer_profile">
+      </div>						
+    </div><!--- END COL -->							
+  </div><!--- END ROW -->					
+</div><!--- END CONTAINER -->
+</div>`;
+
+// const scrol = document.getElementById("scrollPage");
+
+// window.onscroll = function (){
+//   let Y = window.scrollY;
+//   scrol.style.transform = "translateY(" + Y / 2 + "px)";
+// }
